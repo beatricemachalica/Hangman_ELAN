c8de58a00922d31277a197f3ed0ec41523a94e47
beatricemachalica <beatka.macha68@yahoo.fr>
2021-03-12 10:49:37 +0100

    com update 12-03

--- js/app.js
+++ js/app.js
@@ -24,6 +24,8 @@ function pickWord() {
     Math.floor(Math.random() * words_liste.length)
   ].toUpperCase();
 }
+// On execute la fonction :
+pickWord();
 
 // toUpperCase() convertie en majuscules.
 
@@ -34,35 +36,16 @@ function pickWord() {
 // selon une distribution approximativement uniforme sur cet intervalle.
 // Ce nombre peut ensuite être multiplié afin de couvrir un autre intervalle.
 
-// On execute la fonction :
-pickWord();
-
+// état de la partie :
 let status = answer.split("").map((letter) => "_");
 
-// generate underscores
-function underscores() {
-  for (let i = 0; i < status.length; i++) {
-    document.getElementById("underscoreDiv").innerHTML +=
-      `<span class="letterSpan">${status[i]}</span>` + ` `;
-  }
-}
-underscores();
-console.log(answer);
+// split() permet de diviser une chaîne de caractères à partir d'un séparateur pour fournir un tableau de sous-chaînes.
 
-// keyboard generator
-// autre solution pour une fonction compare qui est directement dans les buttons
-// : map((letter) => `<button id='` + letter + `' onClick="compare('` + letter + `')">` + letter +`</button>`)
-// .map permet de recréer un tableau à partir d'un tableau
-function lettersListe() {
-  let buttonsHTML = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    .split("")
-    .map((letter) => `<button id='` + letter + `'>` + letter + `</button>`)
-    .join("");
-  document.getElementById("letters").innerHTML = buttonsHTML;
-}
+// .map permet de recréer un tableau à partir d'un tableau :
+
 // L'objet Map représente un dictionnaire, autrement dit une carte de clés/valeurs.
-// N'importe quelle valeur valable en JavaScript
-// (que ce soit les objets ou les valeurs de types primitifs) peut être utilisée comme clé ou comme valeur.
+// N'importe quelle valeur valable en JavaScript (que ce soit les objets ou les valeurs de types primitifs)
+// peut être utilisée comme clé ou comme valeur.
 // L'ordre d'insertion des clés est mémorisé dans l'objet et les boucles sur les Map parcourent les clés dans cet ordre.
 
 // exemple de son utilisation :
@@ -72,7 +55,35 @@ function lettersListe() {
 // console.log(map1);
 // expected output: Array [2, 8, 18, 32]
 
+// generate underscores :
+function underscores() {
+  for (let i = 0; i < status.length; i++) {
+    document.getElementById("underscoreDiv").innerHTML +=
+      `<span class="letterSpan">${status[i]}</span>` + ` `;
+  }
+}
+// On execute la fonction :
+underscores();
+console.log(answer);
+
+// keyboard generator :
+
+// autre solution pour une fonction compare qui est directement dans les buttons
+// : map((letter) => `<button id='` + letter + `' onClick="compare('` + letter + `')">` + letter +`</button>`)
+
+function lettersListe() {
+  let buttonsHTML = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    .split("")
+    .map((letter) => `<button id='` + letter + `'>` + letter + `</button>`)
+    .join("");
+  document.getElementById("letters").innerHTML = buttonsHTML;
+}
+// On execute la fonction :
 lettersListe();
+
+// (voir explication de .map plus haut)
+// join() crée et renvoie une nouvelle chaîne de caractères en concaténant tous les éléments d'un tableau (ou d'un objet semblable à un tableau).
+// La concaténation utilise la virgule ou une autre chaîne, fournie en argument, comme séparateur.
 
 const lettersKeys = document.querySelectorAll("#letters button");
 
